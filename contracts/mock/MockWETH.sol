c5666779b1f52d47f19273f6223faa94acb6fe94
robercano <3525807+robercano@users.noreply.github.com>
2021-10-25 17:55:29 +0200

    Mint for a list of recepients

--- contracts/mock/MockWETH.sol
+++ contracts/mock/MockWETH.sol
@@ -4,7 +4,9 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockWETH is ERC20 {
-    constructor() ERC20("MockWETH", "WETH") {
-        _mint(_msgSender(), 100 * (10**18));
+    constructor(address[] memory recipients) ERC20("MockWETH", "WETH") {
+        for (uint256 i = 0; i < recipients.length; ++i) {
+            _mint(recipients[i], 1000 * (10**18));
+        }
     }
 }
