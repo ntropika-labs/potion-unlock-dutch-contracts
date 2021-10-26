900e8490dd98e72af576b48cd7a714291ccdc59c
robercano <3525807+robercano@users.noreply.github.com>
2021-10-26 16:16:33 +0200

    Added auction+game button

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -6,19 +6,38 @@ import { useWallet } from "@binance-chain/bsc-use-wallet";
 import NFTValidator from "../../components/NFTValidator";
 import NFTAuction from "../../components/NFTAuction";
 import Decrypt from "../../components/Decrypt";
+import { useCallback, useState } from "react";
 
 const Main: React.FC<any> = props => {
     const { account } = useWallet();
+
+    const [showAuction, setShowAuction] = useState<boolean>(true);
+    const handleShowAuction = useCallback(() => {
+        setShowAuction(!showAuction);
+    }, [showAuction, setShowAuction]);
 
     return (
         <div>
             <Wallet />
 
-            {/* {account && <PublicKey />} */}
-            {account && <NFTAuction />}
-            {/* {account && <Decrypt />}
-            {account && <NFTContract />}
-            {account && <NFTValidator />} */}
+            {account && (
+                <div>
+                    <button type="button" className="btn btn-primary" onClick={handleShowAuction}>
+                        {showAuction ? "Show Game" : "Show Auction"}
+                    </button>
+
+                    {showAuction ? (
+                        <NFTAuction />
+                    ) : (
+                        <div>
+                            <PublicKey />
+                            <Decrypt />
+                            <NFTContract />
+                            <NFTValidator />
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
