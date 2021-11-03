f706b2e78bb9a6f5d5b1f44e872498b9ab07ecf9
Alberto <albertopastrolin@gmail.com>
2021-11-03 12:16:34 +0100

    Added INFTPotionValidator to the abis generation

--- hardhat.config.js
+++ hardhat.config.js
@@ -35,7 +35,7 @@ module.exports = {
         path: './abis',
         clear: true,
         flat: true,
-        only: [':NFT'],
+        only: [':NFT', ':INFTPotionValidator$'],
         spacing: 2,
         pretty: false,
       }
