f25068d8744dd5d6401c960ef5ce40335913013d
robercano <3525807+robercano@users.noreply.github.com>
2021-11-08 12:35:26 +0100

    Enabled solc optimizations

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,7 +21,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: "0.8.9",
+    solidity: {
+        version: "0.8.9",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
     paths: {
         artifacts: "./src/artifacts",
     },
