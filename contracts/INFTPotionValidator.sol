e4203e326a2b1b49a789316335638febcbc4bddd
robercano <3525807+robercano@users.noreply.github.com>
2021-11-15 10:18:46 +0100

    Removed unused file

--- contracts/INFTPotionValidator.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.9;
-
-interface INFTPotionValidator {
-    /**
-        Events
-     */
-    event NFTValidated(address owner, uint256 tokenId);
-}
