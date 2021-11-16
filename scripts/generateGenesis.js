99a89552a7c715c762b1a337e50f712c92379ae8
robercano <3525807+robercano@users.noreply.github.com>
2021-11-16 12:10:30 +0100

    Generate password genesis along original genesis

--- scripts/generateGenesis.js
+++ scripts/generateGenesis.js
@@ -1,20 +1,31 @@
 const { randomBytes } = require("./lib/nacl.js");
 const { bufferToHex } = require("ethereumjs-util");
+const { encryptPassword } = require("./lib/utils.js");
+
 const yargs = require("yargs");
 
 async function main() {
     const argv = await yargs
-        .option("secretSize", {
+        .option("genesisSize", {
             alias: "s",
-            description: "Size in bytes of the secret to be generated",
+            description: "Size in bytes of the original genesis to be generated",
             type: "number",
         })
-        .demandOption(["secretSize"])
+        .demandOption(["genesisSize"])
         .help()
         .alias("help", "h").argv;
 
-    const secret = bufferToHex(randomBytes(argv.secretSize));
-    console.log(secret);
+    const originalGenesis = bufferToHex(randomBytes(argv.genesisSize));
+    const passwordGenesis = bufferToHex(randomBytes((argv.genesisSize - 72) / 2));
+
+    const encryptedPasswordGenesis = encryptPassword(passwordGenesis.substr(2));
+
+    if (originalGenesis.length !== encryptedPasswordGenesis.length) {
+        throw new Error("Cannot generate a proper password genesis for the given genesis size");
+    }
+
+    console.log(`POTION_GENESIS=${originalGenesis}`);
+    console.log(`PASSWORD_GENESIS=${passwordGenesis}`);
 }
 
 main()
