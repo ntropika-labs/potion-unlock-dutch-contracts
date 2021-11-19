b09fbaf78da6f9249a46337c04c08679cf0fb2e7
robercano <3525807+robercano@users.noreply.github.com>
2021-11-19 19:08:16 +0100

    Added app env variables for web3 provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import { withGlobalState } from "react-globally";
 import { NFTPotionContractProvider } from "./contexts/NFTPotionContractProvider";
 import { NFTValidatorContractProvider } from "./contexts/NFTValidatorContractProvider";
 import { NFTAuctionContractProvider } from "./contexts/NFTAuctionContractProvider";
-
 import { UseWalletProvider } from "@binance-chain/bsc-use-wallet";
-import { ChainId } from "./utils/provider";
 
 import Main from "./views/Main";
 
 const App: React.FC<any> = props => {
     return (
-        <UseWalletProvider chainId={ChainId}>
+        <UseWalletProvider
+            chainId={process.env.REACT_APP_CHAIN_ID !== "" ? parseInt(process.env.REACT_APP_CHAIN_ID) : 1337}
+        >
             <NFTPotionContractProvider>
                 <NFTValidatorContractProvider>
                     <NFTAuctionContractProvider>
--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -1,15 +1,18 @@
-import { ethers } from 'ethers';
-import { web3ProviderFrom } from './ether-utils';
-
-export const ChainId = 1337;
+import { ethers } from "ethers";
+import { web3ProviderFrom } from "./ether-utils";
 
 export function getDefaultProvider(): ethers.providers.Web3Provider {
-    return new ethers.providers.Web3Provider(
-        web3ProviderFrom('http://localhost:8545'),
-        ChainId
-    );
+    const ChainID = process.env.REACT_APP_CHAIN_ID !== undefined ? parseInt(process.env.REACT_APP_CHAIN_ID) : 1337;
+    const Provider =
+        process.env.REACT_APP_WEB3_PROVIDER_URL !== undefined
+            ? process.env.REACT_APP_WEB3_PROVIDER_URL
+            : "https://localhost:8545";
+    console.log(`Provider: ${Provider}`);
+    console.log(`ChainID: ${ChainID}`);
+    return new ethers.providers.Web3Provider(web3ProviderFrom(Provider), ChainID);
 }
 
 export function getWeb3Provider(provider: any): ethers.providers.Web3Provider {
-    return new ethers.providers.Web3Provider(provider, ChainId);
+    const ChainID = process.env.REACT_APP_CHAIN_ID !== "" ? parseInt(process.env.REACT_APP_CHAIN_ID) : 1337;
+    return new ethers.providers.Web3Provider(provider, ChainID);
 }
