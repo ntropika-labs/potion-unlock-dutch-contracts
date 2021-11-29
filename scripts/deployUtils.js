5e4343959db46cace4c98c930de9ff09d491a1f5
robercano <3525807+robercano@users.noreply.github.com>
2021-11-29 10:56:48 +0100

    Added deployment utils + script for V2

--- scripts/deployUtils.js
+++ scripts/deployUtils.js
@@ -53,6 +53,27 @@ async function deployNFTContract(NFTAuctionContract, secret, rarityConfig, enabl
     return NFTPotion;
 }
 
+async function deployNFTV2Contract(secret, rarityConfig, enableExport = true) {
+    const NFTPotionV2Factory = await ethers.getContractFactory("NFTPotionV2");
+    let NFTPotionV2 = await NFTPotionV2Factory.deploy(
+        NFT_NAME,
+        NFT_SYMBOL,
+        IPFS_PREFIX,
+        IPFS_SUFFIX,
+        secret,
+        rarityConfig,
+    );
+
+    await NFTPotionV2.deployed();
+
+    console.log("Potion NFT V2 Contract deployed to:", NFTPotionV2.address);
+    if (enableExport) {
+        exportContract("NFTV2Potion", NFTPotionV2.address);
+    }
+
+    return NFTPotionV2;
+}
+
 async function deployNFTValidator(NFTContractAddress, merkleTree, secret, enableExport = true) {
     const merkleRoot = merkleTree.getHexRoot();
 
@@ -114,4 +135,46 @@ async function deployPotionNFTGame(showLogs = true, enableExport = true) {
     return { NFTAuction, NFTPotion, NFTValidator, encryptedPassword };
 }
 
-module.exports = { deployAuction, deployNFTContract, deployNFTValidator, deployPotionNFTGame };
+async function deployPotionNFTV2Game(showLogs = true, enableExport = true) {
+    if (showLogs) {
+        console.log = EnableConsoleLog;
+    } else {
+        console.log = DisableConsoleLog;
+    }
+
+    // Rarities
+    const raritiesConfig = getRaritiesConfig();
+    const raritiesConfigSolidity = encodeRarityConfig(raritiesConfig);
+
+    // Genesis
+    const potionGenesis = getPotionGenesis();
+
+    // Source code password
+    const encryptedPassword = encryptPassword(process.env.PASSWORD_GENESIS);
+    const encryptedPasswordLength = Buffer.from(encryptedPassword.slice(2), "hex").length;
+
+    if (encryptedPasswordLength !== potionGenesis.length) {
+        console.log(
+            `Encrypted source code password length (${encryptedPasswordLength}) is different from Potion Genesis length (${potionGenesis.length})`,
+        );
+        return;
+    }
+
+    console.log(`\nEncrypted Password: ${encryptedPassword}\n\n`);
+
+    // Merkle tree
+    const merkleTree = buildMerkleTree(potionGenesis, raritiesConfig);
+    console.log(`Merkle Tree root: ${merkleTree.getHexRoot()}\n\n`);
+
+    // NFT V2 contract
+    const NFTPotionV2 = await deployNFTV2Contract(encryptedPassword, raritiesConfigSolidity, enableExport);
+
+    // Validator contract
+    const NFTValidator = await deployNFTValidator(NFTPotionV2.address, merkleTree, potionGenesis, enableExport);
+
+    console.log = EnableConsoleLog;
+
+    return { NFTPotionV2, NFTValidator, encryptedPassword };
+}
+
+module.exports = { deployAuction, deployNFTContract, deployNFTValidator, deployPotionNFTGame, deployPotionNFTV2Game };
