e07c1cde71800f3bb98cc1c2ee383064f2cfb6fc
robercano <3525807+robercano@users.noreply.github.com>
2022-02-04 12:32:51 +0100

    Fix encrypting files in different paths

--- scripts/encryptRepository.js
+++ scripts/encryptRepository.js
@@ -1,5 +1,6 @@
 const { encryptDataSymmetric } = require("./lib/utils");
 const { readFileSync, writeFileSync } = require("fs");
+const path = require("path");
 
 const yargs = require("yargs");
 
@@ -14,18 +15,19 @@ async function main() {
         .help()
         .alias("help", "h").argv;
 
+    const basename = path.basename(argv.file);
     const data = readFileSync(argv.file);
     const encryptedFile = encryptDataSymmetric(data);
-    const filenameNoExt = argv.file.split(".").slice(0, -1).join(".");
+    const filenameNoExt = basename.split(".").slice(0, -1).join(".");
 
     // Create the header consisting of the length of the original filename
     // plus the original filename.
-    const blob = Buffer.alloc(4 + argv.file.length + encryptedFile.length);
-    blob.writeUInt32LE(argv.file.length, 0);
-    Buffer.from(argv.file).copy(blob, 4);
+    const blob = Buffer.alloc(4 + basename.length + encryptedFile.length);
+    blob.writeUInt32LE(basename.length, 0);
+    Buffer.from(basename).copy(blob, 4);
 
     // Then insert the encrypted file
-    Buffer.from(encryptedFile).copy(blob, 4 + argv.file.length);
+    Buffer.from(encryptedFile).copy(blob, 4 + basename.length);
 
     writeFileSync(filenameNoExt + ".crypto", blob);
 }
